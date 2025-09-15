17c92b2c3c2660f4c0659eee8816a678126768da
meghanaatmakuri <meghanaatmakuri7@gmail.com>
2025-09-15 12:10:59 +0530

    made changes

---
+++ api/health.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  // Only allow GET requests
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  res.status(200).json({ status: 'ok' });
+}
